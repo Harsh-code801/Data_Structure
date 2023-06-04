609640a2e566c1a7e802f3be522d58231c511934
harsh <harsh@DESKTOP-2GQ2PKH>
2023-06-04 19:17:52 +0530

    Is Non Decrising Array

--- Data Structure/Data Structure/Arrays/Array.cs
+++ Data Structure/Data Structure/Arrays/Array.cs
@@ -78,5 +78,22 @@ namespace Data_Structure.Arrays
             arr[0] = lastnumber;
             return arr;
         }
+        public bool IsNonDecrisingArray(int[] arr,int length)
+        {
+            bool isChange = false;
+            for(int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] <= arr[i + 1])
+                    continue;
+                if(isChange)
+                    return false;
+                if(i==0 || arr[i + 1] >= arr[i-1])
+                    arr[i] = arr[i+1];
+                else
+                    arr[i+1] = arr[i];
+                isChange = true;
+            }
+            return true;
+        }
     }
 }
--- Data Structure/Data Structure/Program.cs
+++ Data Structure/Data Structure/Program.cs
@@ -15,8 +15,16 @@ namespace DataStructure
             secondLargest = new int[] { 10, 10, 100 };
             //Console.WriteLine("Second Largest Number Is " + array.SecondLargestNumber(secondLargest, secondLargest.Length));
             #endregion
+
+            #region RotateElementByK
             int[] rotateArray = new int[] { 100, -3, 95, 100, 55, 177, 3, -4, -3, 95, 100, 95, 177, -5, -4, 177, 101 };
-            array.RotateElementByK(rotateArray, rotateArray.Length,5);
+            array.RotateElementByK(rotateArray, rotateArray.Length, 5);
+            #endregion
+
+            #region NonDecrisingArray
+            int[] nonDecrising = new int[] { 4, 2, 1 };
+            Console.WriteLine("IsNonDecrisingArray: " + array.IsNonDecrisingArray(nonDecrising, nonDecrising.Length));
+            #endregion
         }
     }
 }
