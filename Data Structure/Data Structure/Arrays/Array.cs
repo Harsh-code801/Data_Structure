f8467c11c487f5beb5bca00326036ef4b2452b96
harsh <harsh@DESKTOP-2GQ2PKH>
2023-06-04 11:41:28 +0530

    Second Largest Number

---
+++ Data Structure/Data Structure/Arrays/Array.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure.Arrays
+{
+    internal class Array
+    {
+        public int SecondLargestNumber(int[] arr,int length)
+        {
+            //#region First Solution
+            //arr = arr.Distinct().Reverse().ToArray();
+            //if (arr.Length > 3)
+            //    return arr[1];
+            //#endregion
+            int firstLargest = 0;
+            int secondLargest = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > firstLargest)
+                {
+                    secondLargest = firstLargest;
+                    firstLargest = arr[i];
+                }
+                else if (arr[i] < firstLargest && arr[i] > secondLargest)
+                    secondLargest = arr[i];
+            }
+            if(secondLargest == 0)// check is all items in array are same
+            {
+                int temp = arr[0];
+                bool sameNumber = true;
+                foreach(int n in arr)
+                {
+                    if(temp != n)
+                        sameNumber = false;
+                }
+                if (sameNumber)
+                    Console.WriteLine("Second Largest Number not found");
+            }
+            return secondLargest;
+        }
+    }
+}
--- Data Structure/Data Structure/Program.cs
+++ Data Structure/Data Structure/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data_Structure.Arrays;
+using System;
 
 namespace DataStructure
 {
@@ -6,7 +7,15 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            
+            Data_Structure.Arrays.Array array = new Data_Structure.Arrays.Array();
+
+
+            #region SecondLargestNumber
+            int[] secondLargest = new int[] { 100, -3, 95, 100, 95, 177, -5, -4, 177, 101 };
+            secondLargest = new int[] { 10, 10, 100 };
+            Console.WriteLine("Second Largest Number Is " + array.SecondLargestNumber(secondLargest, secondLargest.Length)); 
+            #endregion
+
         }
     }
 }
