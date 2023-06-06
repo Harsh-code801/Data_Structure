f4af2a4374aac9eb1c3af6f0c5e977293f948ef6
harsh <harsh@DESKTOP-2GQ2PKH>
2023-06-06 14:32:13 +0530

    Reverse String By Words

--- Data Structure/Data Structure/Program.cs
+++ Data Structure/Data Structure/Program.cs
@@ -7,40 +7,55 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Data_Structure.Arrays.Array array = new Data_Structure.Arrays.Array();
+            if (false)
+            {
+                #region Arrays
+                Data_Structure.Arrays.Array array = new Data_Structure.Arrays.Array();
 
+                #region SecondLargestNumber
+                int[] secondLargest = new int[] { 100, -3, 95, 100, 95, 177, -5, -4, 177, 101 };
+                secondLargest = new int[] { 10, 10, 100 };
+                //Console.WriteLine("Second Largest Number Is " + array.SecondLargestNumber(secondLargest, secondLargest.Length));
+                #endregion
 
-            #region SecondLargestNumber
-            int[] secondLargest = new int[] { 100, -3, 95, 100, 95, 177, -5, -4, 177, 101 };
-            secondLargest = new int[] { 10, 10, 100 };
-            //Console.WriteLine("Second Largest Number Is " + array.SecondLargestNumber(secondLargest, secondLargest.Length));
-            #endregion
+                #region RotateElementByK
+                int[] rotateArray = new int[] { 100, -3, 95, 100, 55, 177, 3, -4, -3, 95, 100, 95, 177, -5, -4, 177, 101 };
+                array.RotateElementByK(rotateArray, rotateArray.Length, 5);
+                #endregion
 
-            #region RotateElementByK
-            int[] rotateArray = new int[] { 100, -3, 95, 100, 55, 177, 3, -4, -3, 95, 100, 95, 177, -5, -4, 177, 101 };
-            array.RotateElementByK(rotateArray, rotateArray.Length, 5);
-            #endregion
+                #region NonDecrisingArray
+                int[] nonDecrising = new int[] { 4, 2, 1 };
+                Console.WriteLine("IsNonDecrisingArray: " + array.IsNonDecrisingArray(nonDecrising, nonDecrising.Length));
+                #endregion
 
-            #region NonDecrisingArray
-            int[] nonDecrising = new int[] { 4, 2, 1 };
-            Console.WriteLine("IsNonDecrisingArray: " + array.IsNonDecrisingArray(nonDecrising, nonDecrising.Length));
-            #endregion
+                #region Equilibrium
+                int[] Equilibrium = new int[] { -7, 1, 5, 2, -4, 3, 0 };
+                Equilibrium = new int[] { 1, 2, 3, 3, 2, 1 };
+                //Equilibrium = new int[] { 1,2,-2,-1};
+                Console.WriteLine("Equilibrium Index: " + array.Equilibrium(Equilibrium, Equilibrium.Length));
+                #endregion
 
-            #region Equilibrium
-            int[] Equilibrium = new int[] { -7, 1, 5, 2, -4, 3, 0 };
-            Equilibrium = new int[] { 1, 2, 3, 3, 2, 1 };
-            //Equilibrium = new int[] { 1,2,-2,-1};
-            Console.WriteLine("Equilibrium Index: " + array.Equilibrium(Equilibrium, Equilibrium.Length));
-            #endregion
+                #region First Missing Posative Number
+                int[] firstMissingPosativeNumber = new int[] { 3, 4, -1, 1 };
+                firstMissingPosativeNumber = new int[] { 2, -9, 5, 11, 1, -10, 7 };
+                firstMissingPosativeNumber = new int[] { 4, 6, 5, 3, 8 };
+                firstMissingPosativeNumber = new int[] { 2, 1, 0, 4 };
+                firstMissingPosativeNumber = new int[] { 0, 0, 2, 1 };
+                firstMissingPosativeNumber = new int[] { 1, 2, 3, 4 };
+                Console.WriteLine("First Missing Posative Value :" + array.firstMissingPosativeNumber(firstMissingPosativeNumber, firstMissingPosativeNumber.Length));
+                #endregion
 
-            #region First Missing Posative Number
-            int[] firstMissingPosativeNumber = new int[] { 3, 4, -1, 1 };
-            firstMissingPosativeNumber = new int[] { 2, -9, 5, 11, 1, -10, 7 };
-            firstMissingPosativeNumber = new int[] { 4, 6, 5, 3, 8 };
-            firstMissingPosativeNumber = new int[] { 2, 1, 0, 4 };
-            firstMissingPosativeNumber = new int[] { 0, 0, 2, 1 };
-            firstMissingPosativeNumber = new int[] { 1, 2, 3, 4 };
-            Console.WriteLine("First Missing Posative Value :" + array.firstMissingPosativeNumber(firstMissingPosativeNumber, firstMissingPosativeNumber.Length));
+                #endregion 
+            }
+
+            #region String
+            Data_Structure.Strings.String stringOperaations = new Data_Structure.Strings.String();
+
+            string reverseStringByWords = "Hello My Name Is Groot";
+            reverseStringByWords = "I love programming very much";
+            reverseStringByWords = "a good   example";
+            Console.WriteLine(stringOperaations.ReverseStringWordWise(reverseStringByWords));
+
             #endregion
         }
     }
---
+++ Data Structure/Data Structure/Strings/String.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure.Strings
+{
+    internal class String
+    {
+        public string ReverseStringWordWise(string inputeString)
+        {
+            string result = string.Empty;
+            string[] splitString =  inputeString.Split(" ");
+
+            for(int i = splitString.Length - 1; i >= 0; i--)
+            {
+                if (!string.IsNullOrEmpty(splitString[i]))
+                    result = string.Join(" ", result, splitString[i]);
+            }
+            return result.Trim(' ');
+        }
+    }
+}
