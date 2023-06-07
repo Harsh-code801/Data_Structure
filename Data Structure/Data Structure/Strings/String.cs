2c1219083fb63b409951f995201599cdd8d59ca2
harsh <harsh@DESKTOP-2GQ2PKH>
2023-06-07 13:49:49 +0530

    Remove comment code

--- Data Structure/Data Structure/Strings/String.cs
+++ Data Structure/Data Structure/Strings/String.cs
@@ -112,47 +112,6 @@ namespace Data_Structure.Strings
                 }
             }
             return Math.Min(t1,t2);
-
-
-
-
-
-
-            //int t1 = 0, t2 = 0;
-            //char s1 = '0', s2 = '1';
-
-
-
-
-            //for (int i = 0; i < inputString.Length; i++)
-            //{
-            //    // Iterate over the string 
-            //    if (inputString[i] == '1')
-            //    {
-            //        if (s1 == '0')
-            //            t1++;
-            //        else
-            //            t2++;
-            //    }
-            //    else
-            //    {
-            //        if (s1 == '1')
-            //            t1++;
-            //        else
-            //            t2++;
-            //    }
-            //    if (s1 == '1')
-            //    {
-            //        s1 = '0';
-            //        s2 = '1';
-            //    }
-            //    else
-            //    {
-            //        s1 = '1';
-            //        s2 = '0';
-            //    }
-            //}
-            //return Math.Min(t1, t2);
         }
     }
 }
